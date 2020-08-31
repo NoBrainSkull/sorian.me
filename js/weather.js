5885e5df98841563d7d5d91c06944afd15bc231c
cprice <cprice@tutanota.com>
2020-08-31 21:53:36 +0200

    Add personal preferences

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 50.629250;
+    let longitude = 3.057256;
 
     getWeather(latitude, longitude);
 }
